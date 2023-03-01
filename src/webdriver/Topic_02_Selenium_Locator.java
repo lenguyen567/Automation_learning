e468e96fb98568c3a6d8e112c803142cd778bf3c
lenguyen <ngthile567@gmail.com>
2023-03-01 22:54:23 +0700

    Update template

---
+++ src/webdriver/Topic_02_Selenium_Locator.java
@@ -0,0 +1,52 @@
+package webdriver;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+//import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_02_Selenium_Locator {
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+	String osName = System.getProperty("os.name");
+
+	@BeforeClass
+	public void beforeClass() {
+		if (osName.contains("Windows")) {
+//			System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
+			System.setProperty("webdriver.chrome.driver", projectPath + "/browserDrivers/chromedriver");
+		} else {
+//			System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
+			System.setProperty("webdriver.chrome.driver", projectPath + "/browserDrivers/chromedriver");
+		}
+
+//		driver = (WebDriver) new FirefoxDriver();
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com/");
+	}
+
+	@Test
+	public void TC_01_Url() {
+	}
+
+	@Test
+	public void TC_02_Logo() {
+	}
+
+	@Test
+	public void TC_03_Form() {
+		
+	}
+//close after open the page
+//	@AfterClass
+//	public void afterClass() {
+//		driver.quit();
+//	}
+//	change repo;
+}
